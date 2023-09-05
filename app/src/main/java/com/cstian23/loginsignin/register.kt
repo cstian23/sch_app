19dcf4e40b15254bf01337e4f3a112914c327a2c
cstian23 <christiansuherli23@gmail.com>
2023-09-05 16:32:04 +0700

    first commit

---
+++ app/src/main/java/com/cstian23/loginsignin/register.kt
@@ -0,0 +1,11 @@
+package com.cstian23.loginsignin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
